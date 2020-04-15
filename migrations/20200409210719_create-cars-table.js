ffef7dc03e084e11040fd23deea80206619d9b8d
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-04-14 18:52:43 -0700

    Updated the migrations

--- migrations/20200409210719_create-cars-table.js
+++ migrations/20200409210719_create-cars-table.js
@@ -2,10 +2,18 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', tbl => {
         tbl.increments();
-        tbl.integer('VIN').unique().notNullable();
-        tbl.text('make');
-        tbl.text('model');
-        tbl.integer('mileage');
+        tbl.integer('VIN' , 17)
+            .unique()
+            .notNullable();
+        tbl.string('make', 128)
+            .notNullable();
+        tbl.string('model', 128)
+            .notNullable();
+        tbl.integer('mileage')
+            .notNullable();
+        tbl.string('transmission_type', 128);
+        tbl.strng('title_status', 128);
+        tbl.integer('year');
     });
 };
 
